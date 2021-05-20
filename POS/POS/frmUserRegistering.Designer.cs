83c8e6e0dbc082cd86b0bdbf55b54768abf81f5a
Kavinda Yoshana <kyoshana@gmail.com>
2021-05-20 22:00:16 +0530

    Add try catch to user registration

--- POS/POS/frmUserRegistering.Designer.cs
+++ POS/POS/frmUserRegistering.Designer.cs
@@ -277,6 +277,7 @@
             this.button1.Size = new System.Drawing.Size(113, 52);
             this.button1.TabIndex = 16;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- POS/POS/frmUserRegistering.cs
+++ POS/POS/frmUserRegistering.cs
@@ -21,5 +21,18 @@ namespace POS
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
